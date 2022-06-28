6b019e920190e6b01c022d393d8633ae797a35b3
김하경 <khg05311@naver.com>
2022-06-28 13:49:18 +0900

    chap.2 end

--- public/js/SpriteSheet.js
+++ public/js/SpriteSheet.js
@@ -29,12 +29,12 @@ export default class SpriteSheet {
     this.define(name, x*this.width, y*this.height, this.width, this.height)
   }
 
-    draw(name, context, x, y) {
-        const buffer = this.tiles.get(name);
-        context.drawImage(buffer, x, y);
-    }
+  draw(name, context, x, y) {
+      const buffer = this.tiles.get(name);
+      context.drawImage(buffer, x, y);
+  }
 
-    drawTile(name, context, x, y) {
-        this.draw(name, context, x * this.width, y * this.height);
-    }
+  drawTile(name, context, x, y) {
+      this.draw(name, context, x * this.width, y * this.height);
+  }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,20 @@
-import SpriteSheet from './SpriteSheet.js'
+import Compositor from './Compositor.js'
 import { loadImage, loadLevel } from './loaders.js'
 import {loadBackgroundSprites,loadMarioSprites} from './sprites.js'
+import {createBackgroundLayer} from './layers.js'
 
-function drawBackground(background, context, sprites ) {
-  background.ranges.forEach(([x1, x2, y1, y2]) => {
-    for (let x = x1; x < x2; ++x) {
-      for (let y = y1; y < y2; ++y) {
-        sprites.drawTile(background.tile, context, x, y)
-      }
-    }
-  })
-}
 
 
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
 
-class Compositor {
-  constructor() {
-    this.layers = []
-  }
 
-  draw(context) {
-    this.layers.forEach(layer => {
-      layer(context)
-    })
-  }
-}
 
-function createBackgroundLayer(backgrounds, sprites) {
-  const buffer = document.createElement('canvas')
-  buffer.width = 256
-  buffer.height = 240
-
-  backgrounds.forEach(background => {
-    drawBackground(background, buffer.getContext('2d'), sprites)
-  })
-
-  return function drawBackgroundLayer(context) {
-    context.drawImage(buffer,0,0)
+function createSpriteLayer(sprite, pos) {
+  return function drawSpriteLayer(context) {
+    for (let i = 0; i < 20; ++i) {
+      sprite.draw('idle', context, pos.x + i * 16, pos.y)
+    }
   }
 }
 
@@ -47,16 +23,19 @@ Promise.all([
   loadBackgroundSprites(),
   loadLevel('1-1')
 ])
-  .then(([marioSprite,sprites,level]) => {
+  .then(([marioSprite,backgroundSprites,level]) => {
     const comp = new Compositor()
 
-    const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites)
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites)
     comp.layers.push(backgroundLayer)
 
     const pos = {
-      x:64,
-      y:64
+      x:0,
+      y:0
     }
+    
+    const spriteLayer = createSpriteLayer(marioSprite, pos)
+    comp.layers.push(spriteLayer)
 
     function update() {
       comp.draw(context)
