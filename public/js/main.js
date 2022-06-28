73743ef221898385645c02002241baa7e9b8fcab
김하경 <khg05311@naver.com>
2022-06-28 11:53:47 +0900

    20:40

--- public/js/main.js
+++ public/js/main.js
@@ -16,19 +16,56 @@ function drawBackground(background, context, sprites ) {
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
 
+class Compositor {
+  constructor() {
+    this.layers = []
+  }
+
+  draw(context) {
+    this.layers.forEach(layer => {
+      layer(context)
+    })
+  }
+}
+
+function createBackgroundLayer(backgrounds, sprites) {
+  const buffer = document.createElement('canvas')
+  buffer.width = 256
+  buffer.height = 240
+
+  backgrounds.forEach(background => {
+    drawBackground(background, buffer.getContext('2d'), sprites)
+  })
+
+  return function drawBackgroundLayer(context) {
+    context.drawImage(buffer,0,0)
+  }
+}
+
 Promise.all([
   loadMarioSprites(),
   loadBackgroundSprites(),
   loadLevel('1-1')
 ])
   .then(([marioSprite,sprites,level]) => {
-    level.backgrounds.forEach(background => {
-      drawBackground(background, context, sprites)
-    })
+    const comp = new Compositor()
+
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites)
+    comp.layers.push(backgroundLayer)
+
     const pos = {
-      x:164,
-      y:164
+      x:64,
+      y:64
     }
-    marioSprite.draw('idle', context, pos.x, pos.y)
+
+    function update() {
+      comp.draw(context)
+      marioSprite.draw('idle', context, pos.x, pos.y)
+      pos.x +=2
+      pos.y +=2
+      requestAnimationFrame(update)
+    }
+
+    update()
 }) 
   
